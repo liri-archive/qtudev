b0bc9c6574a32076dbecc598524ee6041b61fc7a
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2018-11-30 00:25:18 +0100

    Manually create artifacts package
    Most files are missing when using CPack, it only adds header files so
    we try a manual solution.

--- .travis/build.sh
+++ .travis/build.sh
@@ -53,5 +53,6 @@ travis_end "install"
 # Package
 travis_start "package"
 msg "Package..."
-make package
+mkdir -p artifacts
+tar czf artifacts/qtudev-artifacts.tar.gz -T install_manifest.txt
 travis_end "package"
